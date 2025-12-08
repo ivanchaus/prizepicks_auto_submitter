5b8db772800d7eca7656bc916381f26da5901ab3
ivanchaus <ivan.y.chau@gmail.com>
2025-12-07 22:25:26 -0800

    Refactor electronAPI exposure in preload.js

--- preload.js
+++ preload.js
@@ -1,15 +1,3 @@
 const { contextBridge, ipcRenderer } = require("electron");
 
-contextBridge.exposeInMainWorld("electronAPI", {
-  startSession: () => ipcRenderer.invoke("start-session"),
-  checkSessionStatus: (sessionId) => ipcRenderer.invoke("session-status", sessionId),
-  submitEntries: (data) => ipcRenderer.invoke("submit-entries", data),
-});
-
-
-// contextBridge.exposeInMainWorld("electronAPI", {
-//   startSession: () => ipcRenderer.invoke("start-session"),
-//   checkSessionStatus: (sessionId) => ipcRenderer.invoke("session-status", sessionId),
-//   submitEntries: (data) => ipcRenderer.invoke("submit-entries", data),
-//   closeSession: (sessionId) => ipcRenderer.invoke("close-session", data)
-// });
+contextBridge.exposeInMainWorld("electron", { ipcRenderer });
